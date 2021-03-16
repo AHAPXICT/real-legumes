40e264bf6ecb0682dc2a3c935a8ed6bf8e1648e3
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-03-16 11:47:08 +0200

    Added router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
-import './App.css';
-import Header from './Components/Header/Header'
-import Index from './Components/Index/Index'
-import About from './Components/About/About'
-import Footer from './Components/Footer/Footer'
+import { BrowserRouter, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Index from "./Components/Index/Index";
+import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
 
 // test slider
-import Slider from './Components/Slider/Slider'
+import Slider from "./Components/Slider/Slider";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      {/* <Index />
-      <Slider /> */}
-      <About />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <BrowserRouter>
+                <Route exact path="/">
+                    <Index />
+                </Route>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+                {/* 
+                  <Slider /> */}
+            </BrowserRouter>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
