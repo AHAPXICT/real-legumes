3cc31e2f607c86710606a2caf7faf008b965be36
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2021-03-16 12:47:18 +0200

    Added PageNotFound

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Index from "./Components/Index/Index";
 import About from "./Components/About/About";
 import Footer from "./Components/Footer/Footer";
+import PageNotFound from "./Components/PageNotFound/PageNotFound"
 
 // test slider
 import Slider from "./Components/Slider/Slider";
@@ -14,14 +15,20 @@ function App() {
         <div className="App">
             <BrowserRouter>
                 <Header />
-                <Route exact path="/">
-                    <Index />
-                </Route>
-                <Route exact path="/about">
-                    <About />
-                </Route>
-                {/* 
-                  <Slider /> */}
+                <Switch>
+                    
+                    <Route exact path="/">
+                        <Index />
+                    </Route>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                    {/* 
+                    <Slider /> */}
+                    <Route to='*'>
+                        <PageNotFound/>
+                    </Route>
+                </Switch>
                 <Footer />
             </BrowserRouter>
         </div>
