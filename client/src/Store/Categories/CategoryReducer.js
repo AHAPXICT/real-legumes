8ba280d3365466643c809cd5923f5b825b4ca69e
Roman <roman.kalitovskyy@gmail.com>
2021-03-22 13:49:15 +0200

    Added category reducer

--- client/src/Store/Categories/CategoryReducer.js
+++ client/src/Store/Categories/CategoryReducer.js
@@ -4,3 +4,15 @@ import { SET_CATEGORIES } from './actions'
 const initialState = {
     categories: []
 }
+
+const categoryReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_CATEGORIES:
+            return {
+                ...state,
+                categories: action.payload
+            }
+        default:
+            return state;
+    }
+}
