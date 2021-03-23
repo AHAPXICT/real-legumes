27519ae5e33879f22543bdbc67102bbaffd788d8
Roman <roman.kalitovskyy@gmail.com>
2021-03-23 18:55:42 +0200

    Added input labels to modal.

--- client/src/Components/CategoryAdminPageContainer/CategoryAdminPage/CategoryAdminPage.jsx
+++ client/src/Components/CategoryAdminPageContainer/CategoryAdminPage/CategoryAdminPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CategoryItem from "./CategoryItem/CategoryItem";
 import Button from "../../Button/Button";
+import CategoryModalDialog from "../../FormModalDialog/CategoryModalDialog";
 
 import s from "./style.module.css";
 
@@ -9,6 +10,13 @@ const CategoryAdminPage = ({ categories }) => {
         <>
             <div className={`container ${s.max_height}`}>
                 <div className={s.category__list}>
+                    <CategoryModalDialog
+                        titleText={"Додати категорію"}
+                        helpText={"Введіть назвку категорії."}
+                        fullWidth={100}
+                        btnText={"Додати"}
+                        fieldText={"Категорія"}
+                    />
                     <div className={s.category__button_section}>
                         <Button text="Додати" mode="primary" />
                     </div>
