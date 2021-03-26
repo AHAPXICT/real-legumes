a5c3ec6eddf5397f9cd1f96fd8e772f54c4ccdba
Roman <roman.kalitovskyy@gmail.com>
2021-03-26 13:35:19 +0200

    Added category count to test_category_list.

--- flask-server/tests/functional/test_categories.py
+++ flask-server/tests/functional/test_categories.py
@@ -1,4 +1,5 @@
 from sqlalchemy import desc
+import json
 
 from real_legumes import create_app
 from real_legumes.real_legumes.models import Category
@@ -10,7 +11,9 @@ app = create_app('testing')
 def test_category_list():
     with app.test_client() as test_client:
         response = test_client.get('/api/categories')
+        objects = json.loads(response.get_data(as_text=True))
         assert response.status_code == 200
+        assert len(Category.query.all()) == len(objects)
 
 
 def test_category_post():
@@ -26,3 +29,7 @@ def test_category_post():
 
         assert response.status_code == 201
         assert category.name == 'test category'
+
+
+# def test_category_detele():
+#     with app.test_client() as test_client:
