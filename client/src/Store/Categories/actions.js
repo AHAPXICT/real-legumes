d6fcbafd2ca3044727358e80db578b305b8b2eab
Roman <roman.kalitovskyy@gmail.com>
2021-03-26 16:16:21 +0200

    Added addCategory action

--- client/src/Store/Categories/actions.js
+++ client/src/Store/Categories/actions.js
@@ -1,10 +1,18 @@
 export const SET_CATEGORIES = 'CATEGORY/SET_CATEGORIES'
+export const ADD_CATEGORY = 'CATEGORY/ADD_CATEGORY'
 export const INPUT_VALUE = 'CATEGORY/INPUT_VALUE'
 
 export const setCategories = (categories) => {
     return {
         type: SET_CATEGORIES,
         payload: categories
+    }
+}
+
+export const addCategory = (category) => {
+    return {
+        type: ADD_CATEGORY,
+        payload: category
     }
 }
 
