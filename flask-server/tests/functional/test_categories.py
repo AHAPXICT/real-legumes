25ae7a822b3d07c0ef7be8ac8df81479aa71978e
Roman <roman.kalitovskyy@gmail.com>
2021-03-26 13:25:49 +0200

    Added test_category_post.

--- flask-server/tests/functional/test_categories.py
+++ flask-server/tests/functional/test_categories.py
@@ -1,3 +1,5 @@
+from sqlalchemy import desc
+
 from real_legumes import create_app
 from real_legumes.real_legumes.models import Category
 
@@ -8,10 +10,19 @@ app = create_app('testing')
 def test_category_list():
     with app.test_client() as test_client:
         response = test_client.get('/api/categories')
-        print(Category.query.all() == response.data)
-        print(response)
-        print(response.data)
         assert response.status_code == 200
 
 
-test_category_list()
+def test_category_post():
+    with app.test_client() as test_client:
+        response = test_client.post(
+            '/api/categories',
+            json=dict(
+                name='test category'
+            )
+        )
+
+        category = Category.query.order_by(desc(Category.id)).first()
+
+        assert response.status_code == 201
+        assert category.name == 'test category'
