ae55796ca302c3dbfb08ad8dece6a944f8926b26
Roman <roman.kalitovskyy@gmail.com>
2021-03-26 11:50:31 +0200

    Added test_new_ingredient.

--- flask-server/tests/unit/test_models.py
+++ flask-server/tests/unit/test_models.py
@@ -1,4 +1,4 @@
-from real_legumes.real_legumes.models import Category
+from real_legumes.real_legumes.models import Category, Ingredient
 from real_legumes import create_app
 
 app = create_app('development')
@@ -7,6 +7,12 @@ app = create_app('development')
 def test_new_category():
     with app.app_context():
         category = Category(name='category')
-        print(category.updated_at)
 
         assert category.name == 'category'
+
+
+def test_new_ingredient():
+    with app.app_context():
+        ingredient = Ingredient(name='ingredient')
+
+        assert ingredient.name == 'ingredient'
