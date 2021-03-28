25b17e8628cce5312e7a78a6ba4efcb5f7fa59fb
Roman <roman.kalitovskyy@gmail.com>
2021-03-28 12:07:43 +0300

    Added styles for modal to separate file.

---
+++ client/src/Components/FormModalDialog/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+        borderColor: "#000",
+    },
+    label: {
+        fontFamily: "Lobster, cursive",
+        color: "#000",
+    },
+});
+
+export default useStyles;
