7491a7ce1be8d075a7c661405b8e36931be93eab
Roman <roman.kalitovskyy@gmail.com>
2021-03-29 13:09:00 +0300

    Changed styles to module and refactor to function for About.

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,36 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import AboutProfile from "./AboutProfile/AboutProfile";
 import img_me from "../../static/images/team/me.jpg";
 import img_mary from "../../static/images/team/mary.jpg";
 
-import "./style.css";
+import s from "./style.module.css";
 
-class About extends React.Component {
-    componentDidMount() {
+const About = () => {
+    useEffect(() => {
         window.scrollTo(0, 0);
-    }
+    });
 
-    render() {
-        return (
-            <section className="team">
-                <div className="container">
-                    <h1 className="team__title">Наша команда</h1>
-                    <h2 className="team__subtitle">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Reiciendis fugiat voluptatem dolorum sint quia
-                        voluptatibus natus minima, a nisi! Quos voluptate harum
-                        error fugiat soluta praesentium cupiditate ex
-                        perspiciatis ea!
-                    </h2>
-                    <div className="team__section">
-                        <AboutProfile profile_image={img_me} />
-                        <AboutProfile profile_image={img_mary} />
-                    </div>
+    return (
+        <section className={s.team}>
+            <div className="container">
+                <h1 className={s.team__title}>Наша команда</h1>
+                <h2 className={s.team__subtitle}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Reiciendis fugiat voluptatem dolorum sint quia voluptatibus
+                    natus minima, a nisi! Quos voluptate harum error fugiat
+                    soluta praesentium cupiditate ex perspiciatis ea!
+                </h2>
+                <div className={s.team__section}>
+                    <AboutProfile profile_image={img_me} />
+                    <AboutProfile profile_image={img_mary} />
                 </div>
-            </section>
-        );
-    }
-}
+            </div>
+        </section>
+    );
+};
 
 export default About;
