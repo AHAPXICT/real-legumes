902fd6fd244a99d1f60ac17de93c6d25aa237000
Roman <roman.kalitovskyy@gmail.com>
2021-03-29 13:10:54 +0300

    Changed styles to module for AboutProfile.

--- client/src/Components/About/AboutProfile/AboutProfile.jsx
+++ client/src/Components/About/AboutProfile/AboutProfile.jsx
@@ -5,16 +5,16 @@ import facebook_logo from "../../../static/images/logo/facebook-24-black.png";
 import instagram_logo from "../../../static/images/logo/instagram-24-black.png";
 import twitter_logo from "../../../static/images/logo/twitter-24-black.png";
 
-import "./style.css";
+import s from "./style.module.css";
 
 const AboutProfile = ({ profile_image }) => {
     return (
-        <div class="team__block">
-            <img className="team__img" src={profile_image} alt="" />
-            <div className="team__descr_block">
-                <div className="team__container">
-                    <h3 className="team__name">Roman Kalitovskyy</h3>
-                    <p className="team__descr">
+        <div class={s.team__block}>
+            <img className={s.team__img} src={profile_image} alt="" />
+            <div className={s.team__descr_block}>
+                <div className={s.team__container}>
+                    <h3 className={s.team__name}>Roman Kalitovskyy</h3>
+                    <p className={s.team__descr}>
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. Tenetur minima autem accusantium rem! Ut,
                         architecto recusandae, laudantium exercitationem
