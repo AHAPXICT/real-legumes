0e94ea79f48a9576866ca09df341dc82b5581c85
Roman <roman.kalitovskyy@gmail.com>
2021-03-29 12:57:18 +0300

    Changed styles to module for header.

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import Nav from './Nav/Nav'
+import React from "react";
+import Nav from "./Nav/Nav";
 
-import './style.css'
-
+import s from "./style.module.css";
 
 const Header = () => {
     return (
-        <header className="header sticky">
+        <header className={`${s.header} ${s.sticky}`}>
             <div className="container">
-                <div className="header__inner">
-                    <div className="header__logo">Справжні лєгуміни</div>
+                <div className={s.header__inner}>
+                    <div className={s.header__logo}>Справжні лєгуміни</div>
                     <Nav is_authenticated={false} />
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
